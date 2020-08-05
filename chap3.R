8baf2623d9ec07680d87164fd8f0441a605f175c
Yuki-Kanamori <y.kanamory@gmail.com>
2020-08-05 17:47:09 +0900

    added fig.4

--- chap3.R
+++ chap3.R
@@ -55,43 +55,94 @@ n = 2
 
 
 # 3-1 図4: 漁場の空間分布 --------------------------------------------------------------
-# とりあえず『4_キチジ漁績〜.xlsx』からデータを読むようなコードにしておき，後から2-3-2から直接求められるように修正を加える
-df_gj = read.xlsx("4_キチジ漁績2006-2018.xlsx", 21) %>% filter(魚種コード == 408) %>% select(漁区, 漁獲量の合計) #日本語入力のためか，renameが使えない
-colnames(df_gj) = c("AREA", "abundance")
-head(df_gj)
-summary(df_gj)
+# load the packages -------------------------------------------------------
+require(tidyr)
+require(dplyr)
+require(plyr)
+require(ggplot2)
+require(maps)
+require(mapdata)
+require(stringr)
+require(cowplot)
 
-# 緯度経度データ
-lonlat = read.csv("area_lonlat.csv")
-summary(lonlat)
-lonlat = lonlat %>% mutate(lon = LON%/%1+LON%%1/60, lat = LAT%/%1+LAT%%1/60) #10進法を60進法に変換
 
-df_gj = left_join(df_gj, lonlat, by = "AREA") %>% na.omit()
-df_gj2 = ddply(df_gj, .(lon, lat), summarize, total = sum(abundance)*0.001) #ここが違う．そもそもnが違うので，平均をとる必要があるのでは？
-summary(df_gj2)
+# please change here -----------------------------------------------------------
+# set working directory
+setwd("/Users/Yuki/Dropbox/業務/キチジ太平洋北部/SA2020")
 
-g = ggplot(df_gj2, aes(x = lon, y = lat))
-p = geom_point()
-c = scale_colour_gradientn(colours = c("black", "blue", "cyan", "green", "yellow", "orange", "red", "darkred"))
-g+p+c
+# lonlat = read.delim("Map.txt", header=T)
+# lonlat2 = read.table("キアンコウ緯度経度.txt", header=T, fileEncoding = "CP932")
+# 
+# gyoseki_2years_ago = read.table("kitiji_gyoseki2018.txt", header=T)
 
-# mapping
-unique(map_data("world")$region)
+###10進法で、LONGとLATのみ。列名もLONGとLATにしてください###
+x <- read.delim("Map.txt", header=T)
+
+
+###地図の入ったテキストファイル###
+z <- read.table("キアンコウ緯度経度.txt", header=T, fileEncoding = "CP932")
+z2 <- read.table("kitiji_gyoseki2018.txt", header=T)
+
+head(z2)
+head(z)
+
+for(i in 1 : nrow(z2)) {
+  z2[i, 10] <- as.numeric(z[which(z2[i, 4]==z[, 2]), 5])
+  z2[i, 11] <- as.numeric(z[which(z2[i, 4]==z[, 2]), 6])
+}
+y <- z2 
+colnames(y) <- c(colnames(y)[1:9], "緯度", "経度")
+
+y[, 12] <- as.numeric(as.character(paste(as.numeric(as.character(substr(y$緯度, 1, 2)))+as.numeric(as.character(substr(y$緯度, 4, 5)))/60)))
+y[, 13] <- as.numeric(as.character(paste(as.numeric(as.character(substr(y$経度, 1, 3)))+as.numeric(as.character(substr(y$経度, 5, 6)))/60)))
+y <- data.frame(AREA = tapply(y$漁区, y$漁区, mean), 
+                LAT = tapply(y[, 12], y$漁区, mean), 
+                LONG = tapply(y[, 13], y$漁区, mean), 
+                catch = tapply(y$漁獲量の合計, y$漁区, sum))
+
+
+quartzFonts(HiraKaku = quartzFont(rep("HiraginoSans-W3", 4)))
+theme_set(theme_cowplot(font_family = "HiraKaku")) 
+
 map = ggplot() + coord_fixed() + xlab("Longitude") + ylab("Latitude")
 world_map = map_data("world")
 region2 = subset(world_map, world_map$region == "Japan")
-local_map = map + geom_polygon(data = region2, aes(x = long, y = lat, group = group), colour = "black", fill = "white") + coord_map(xlim = c(xlim = c(min(df_gj2$lon)-1), max(df_gj2$lon)+5), ylim = c(min(df_gj2$lat)-1, max(df_gj2$lat)+1))
+local_map = map + geom_polygon(data = region2, aes(x = long, y = lat, group = group), colour = "black", fill = "white") + coord_map(xlim = c(140.5, 145.5), ylim = c(35, 43))
 th = theme(panel.grid.major = element_blank(),
-               panel.grid.minor = element_blank(),
-               axis.text.x = element_blank(),
-               axis.text.y = element_blank(),
-               axis.title.x = element_text(size = rel(1.5)),
-               axis.title.y = element_text(size = rel(1.5)),
-               strip.text = element_text(size = rel(1.3)),
-               legend.title = element_text(size = 13))
-p = geom_point(data = df_gj2, aes(x = lon, y = lat, colour = total), shape = 15, size = 2)
+           panel.grid.minor = element_blank(),
+           axis.text.x = element_text(size = rel(1.2), angle = 90),
+           axis.text.y = element_text(size = rel(1.5)),
+           axis.title.x = element_text(size = rel(1.5)),
+           axis.title.y = element_text(size = rel(1.5)),
+           strip.text.x = element_text(size = rel(1.5)),
+           plot.title = element_text(size = rel(1.5)),
+           legend.title = element_text(size = rel(1.2)),
+           legend.text = element_text(size = rel(1.2)))
+# th = theme(panel.grid.major = element_blank(),
+#            panel.grid.minor = element_blank(),
+#            axis.text.x = element_text(size = rel(1.2), angle = 90),
+#            axis.text.y = element_text(size = rel(1.5)),
+#            axis.title.x = element_text(size = rel(1.5)),
+#            axis.title.y = element_text(size = rel(1.5)),
+#            legend.title = element_blank(),
+#            legend.text = element_text(size = rel(1.2)),
+#            strip.text.x = element_text(size = rel(1.5)),
+#            legend.position = c(0.85, 0.8),
+#            legend.background = element_rect(fill = "white", size = 0.4, linetype = "solid", colour = "black"))
+p = geom_point(data = y, aes(x = LONG, y = LAT, colour = catch/1000), shape = 15, size = 4)
 c = scale_colour_gradientn(colours = c("black", "blue", "cyan", "green", "yellow", "orange", "red", "darkred"))
-fig = local_map+theme_bw()+th+p+c+labs(title = "", x = "Longitude", y = "Latitude", colour = "")
+labs = labs(x = "経度", y = "緯度", colour = "漁獲量（トン）")
+fig4 = local_map+theme_bw(base_family = "HiraKakuPro-W3")+th+p+c+labs+
+  geom_hline(yintercept = 40.5, colour="black", linetype = "dashed")+
+  geom_hline(yintercept = 39, colour="black", linetype = "dashed")+
+  geom_hline(yintercept = 38, colour="black", linetype = "dashed")+
+  geom_hline(yintercept = 36.5, colour="black", linetype = "dashed")+
+  annotate("text",label="尻屋崎", x=144.5, y=41, family="HiraKaku", size = 5)+
+  annotate("text",label="岩手", x=144.3, y=39.5, family="HiraKaku", size = 5)+
+  annotate("text",label="金華山", x=144.4, y=38.5, family="HiraKaku", size = 5)+
+  annotate("text",label="房総", x=144.3, y=37, family="HiraKaku", size = 5) 
+ggsave(file = "fig4.png", plot = fig4, units = "in", width = 8.27, height = 11.69)
+
 
 
 
