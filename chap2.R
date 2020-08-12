81bd0ed6409427f35f23b556ceebb3efbc335626
Yuki-Kanamori <y.kanamory@gmail.com>
2020-08-12 12:36:36 +0900

    revised figA3-3

--- chap2.R
+++ chap2.R
@@ -868,8 +868,10 @@ ns3 = ddply(ns, .(year, area), summarize, total_number = sum(number_sel), total_
 ns4 = left_join(ns3 %>% select(-total_biomass) %>% spread(key = area, value = total_number), ns3 %>% select(-total_number) %>% spread(key = area, value = total_biomass), by = "year") 
 ns4 = ns4 %>% mutate(n_rate_number = ns4$北部.x/(ns4$南部.x+ns4$北部.x), n_rate_biomass = ns4$北部.y/(ns4$南部.y+ns4$北部.y))
 
+ns4_2 = ns4 %>% mutate(year = year + 1)
+
 head(trend)  
-trend_ns = left_join(trend, ns4 %>% select(year, n_rate_biomass), by = "year")
+trend_ns = left_join(trend, ns4_2 %>% select(year, n_rate_biomass), by = "year")
 trend_ns = trend_ns %>% mutate(total_n = (trend_ns$total)/1000*trend_ns$n_rate_biomass, total_s = (trend_ns$total)/1000*(1-trend_ns$n_rate_biomass)) %>% select(year, total_n, total_s) %>% gather(key = data, value = biomass_sel, -year) %>% mutate(area = rep(c("北部", "南部"), each = length(unique(trend_ns$year)))) %>% na.omit()
 
 
@@ -893,7 +895,7 @@ th = theme(panel.grid.major = element_blank(),
            legend.position = c(0.1, 0.8),
            legend.background = element_rect(fill = "white", size = 0.4, linetype = "solid", colour = "black"))
 c = scale_fill_manual(values =  c("white", "black"))
-fig_a33 = g+b+lab+c+theme_bw(base_family = "HiraKakuPro-W3")+th+scale_x_continuous(breaks=seq(1996, 2019, by = 2), expand= c(0, 0.5))+scale_y_continuous(expand = c(0,0),limits = c(0, 15))
+fig_a33 = g+b+lab+c+theme_bw(base_family = "HiraKakuPro-W3")+th+scale_x_continuous(breaks=seq(1996, 2020, by = 2), expand= c(0, 0.5))+scale_y_continuous(expand = c(0,0),limits = c(0, 15))
 ggsave(file = "fig_a33.png", plot = fig_a33, units = "in", width = 11.69, height = 8.27)
 
 
